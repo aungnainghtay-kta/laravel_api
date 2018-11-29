e4852262221f25f47956ef451df19485ee43c476
Aung Naing Htay <aungnainghtay.main@gmail.com>
2018-11-29 13:41:48 +0630

    Make relation ship between Product and Review

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Model;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product(){
+    	return $this->belongsTo('App\Model\Product');
+    }
 }
